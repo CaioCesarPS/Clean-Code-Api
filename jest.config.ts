8c395d51a0e24dcb0e7913ceff38e4b9f4456361
Caio_CPS <caio.psouza@robbyson.com>
2021-03-17 20:30:03 -0300

    chore: add jest to lint staged

--- jest.config.ts
+++ jest.config.ts
@@ -4,28 +4,13 @@
  */
 
 export default {
-  roots: ['<rootsDir>/src'],
-
-  // Automatically clear mock calls and instances between every test
-  clearMocks: true,
-
-  // Indicates whether the coverage information should be collected while executing the test
-  // collectCoverage: false,
-
-  // An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-
-  // The directory where Jest should output its coverage files
-  coverageDirectory: "coverage",
-
-  // Indicates which provider should be used to instrument code for coverage
-  coverageProvider: "v8",
-
-  // The test environment that will be used for testing
-  testEnvironment: "node",
-
-  // A map from regular expressions to paths to transformers
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+  ],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
   transform: {
-    '.*\\.ts$' : 'ts-jest'
+    '.+\\.ts$': 'ts-jest'
   },
 };
